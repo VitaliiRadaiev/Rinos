f251ff96dc5a265b813159c579d8515233cf0313
Vitalii Radaiev <hf.chelenger@gamil.com>
2021-06-10 14:15:38 +0300

    fix

--- dist/js/app.js
+++ dist/js/app.js
@@ -1397,33 +1397,40 @@ if($tableTabs) {
 			let btn = item.querySelector('.video-block__play-pause');
 
 			if(video) {
-				btn.addEventListener('click', (e) => {
-					e.preventDefault();
-					togglePlayPause(video,btn);
-				});
-				video.addEventListener('ended', () => {
-					video.pause();
-					btn.firstElementChild.className = 'icon-play2';
-					btn.style.opacity = '1';
-					video.removeAttribute('controls');
-				});
-				video.addEventListener('play', () => {
-					btn.firstElementChild.className = 'icon-pause';
-				});
-				video.addEventListener('pause', () => {
-					btn.firstElementChild.className = 'icon-play2';
-				});
-				video.addEventListener('mouseenter', (e) => { 
-					if(!video.paused) {
-						btn.style.opacity = '1';
-					} 
-				});
-				video.addEventListener('mouseleave', (e) => { 
-					if(!video.paused) {
-						btn.style.opacity = '0';
-					} 
-				});
+				if(video.hasAttribute('data-youtube-url')) {
+					let url = video.dataset.youtubeUrl;
+					btn.addEventListener('click', () => {
+						video.innerHTML = `<iframe src="${url}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+					})
 
+				} else {
+					btn.addEventListener('click', (e) => {
+						e.preventDefault();
+						togglePlayPause(video,btn);
+					});
+					video.addEventListener('ended', () => {
+						video.pause();
+						btn.firstElementChild.className = 'icon-play2';
+						btn.style.opacity = '1';
+						video.removeAttribute('controls');
+					});
+					video.addEventListener('play', () => {
+						btn.firstElementChild.className = 'icon-pause';
+					});
+					video.addEventListener('pause', () => {
+						btn.firstElementChild.className = 'icon-play2';
+					});
+					video.addEventListener('mouseenter', (e) => { 
+						if(!video.paused) {
+							btn.style.opacity = '1';
+						} 
+					});
+					video.addEventListener('mouseleave', (e) => { 
+						if(!video.paused) {
+							btn.style.opacity = '0';
+						} 
+					});
+				}
 			}
 		})
 	}
